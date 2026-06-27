1856efb537abdc20dce2e2855099472f5d91d8f3
Ubuntu <ubuntu@ip-172-31-47-164.ap-south-1.compute.internal>
2026-06-27 14:53:50 +0000

    Initial Terraform project setup

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region  = "ap-south-1"
+cluster_name = "game-theory-cluster"
+vpc_name     = "game-theory-vpc"
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "game-theory-cluster"
+}
+
+variable "vpc_name" {
+  default = "game-theory-vpc"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2" {
+  default = "10.0.2.0/24"
+}
